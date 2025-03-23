b715b1c8a8edfac781c6c76065490c35148051a4
Vishnu MM <vishnumm2003@gmail.com>
2025-03-23 14:04:43 +0530

    calender added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -20,6 +20,7 @@ export default function HomeScreen() {
       <ThemedText type="title">Welcome Dear { name }</ThemedText>
         <HelloWave />
       </ThemedView>
+      <text>Calendar</text>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
