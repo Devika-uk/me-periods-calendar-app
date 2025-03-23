dd38d6d6a8e25c1349374aee700b6d80bf462f2c
Devika UK <devikauk43@gmail.com>
2025-03-23 13:47:07 +0530

    vzf mygtfn

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-      <ThemedText type="title">Welcome { name }</ThemedText>
+      <ThemedText type="title">Welcome dear { name }</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
